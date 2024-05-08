362df099df900b35a59e0b80cc257b4ffe74574d
ohishi_MSI2023 <gpu.kenji.ohishi@gmail.com>
2024-05-08 10:52:51 +0900

    GameAlgo001完成

---
+++ Assets/GPU_RETURNS/Scripts/GameInitial.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class GameInitial : MonoBehaviour
+{
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    static void main()
+    {
+        Screen.SetResolution(1280, 800, false, 60);
+        //Resolution res = Screen.currentResolution;
+        //Screen.SetResolution((int)(res.width*0.8f), (int)(res.height*0.8f), false, 60);
+        //Debug.Log("main");
+    }
+
+    //[RuntimeInitializeOnLoadMethod]
+    //static void OnRuntimeMethodLoad()
+    //{
+    //    Screen.SetResolution(1366, 768, false, 60);
+    //    Debug.Log("OnRuntimeMethodLoad");
+    //}
+}
